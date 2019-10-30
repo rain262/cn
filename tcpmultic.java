2dc357a463b4bfa4135c85a639db8d7899d21fc5
rain262 <57172094+rain262@users.noreply.github.com>
2019-10-30 19:33:27 +0530

    Add files via upload

---
+++ tcpmultic.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.net.*;
+
+public class Client_nt {
+
+public static void main(String args[]) throws IOException{
+
+    InetAddress address=InetAddress.getLocalHost();
+    Socket s1=new Socket("192.168.3.34", 4445);
+    String line=null;
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    BufferedReader is=new BufferedReader(new InputStreamReader(s1.getInputStream()));
+    PrintWriter os=new PrintWriter(s1.getOutputStream());     
+    System.out.println("Client Address : "+address);
+    System.out.println("Enter Data to echo Server ( Enter QUIT to end):");
+
+    String response=null;
+    try{
+	line=br.readLine();
+
+        while(line.compareTo("QUIT")!=0)
+	    {
+                os.println(line);
+                os.flush();
+                response=is.readLine();
+                System.out.println("Server Response : "+response);
+                line=br.readLine();
+
+            }
+
+      }
+    catch(IOException e){
+        e.printStackTrace();
+    System.out.println("Socket read Error");
+    }
+    finally{
+
+        is.close();os.close();br.close();s1.close();
+                System.out.println("Connection Closed");
+
+    }
+
+}
+}
+
