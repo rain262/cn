4fb88258b55686b6b517842526b0aac4c73c881a
rain262 <57172094+rain262@users.noreply.github.com>
2019-10-30 18:43:07 +0530

    Add files via upload

---
+++ scalculator.c
@@ -0,0 +1,84 @@
+﻿#include<sys/types.h>
+#include<sys/socket.h>
+#include<stdio.h>
+#include<netinet/in.h> 
+#include <unistd.h>
+#include<string.h> 
+#include <arpa/inet.h>
+
+void main()
+{
+int b,sockfd,connfd,sin_size,l,n,len;
+char operator;
+int op1,op2,result;
+if((sockfd=socket(AF_INET,SOCK_STREAM,0))>0)
+printf("socket created sucessfully\n");  
+
+
+struct sockaddr_in servaddr;              
+struct sockaddr_in clientaddr;
+
+servaddr.sin_family=AF_INET;
+servaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+servaddr.sin_port=6006;
+
+if((bind(sockfd, (struct sockaddr *)&servaddr,sizeof(servaddr)))==0)
+printf("bind sucessful\n");   
+	
+if((listen(sockfd,5))==0) 
+printf("listen sucessful\n");
+
+
+sin_size = sizeof(struct sockaddr_in);
+if((connfd=accept(sockfd,(struct sockaddr *)&clientaddr,&sin_size))>0);
+printf("accept sucessful\n");
+
+read(connfd, &operator,10);
+read(connfd,&op1,sizeof(op1));
+read(connfd,&op2,sizeof(op2));
+switch(operator) {
+        case '+': result=op1 + op2;
+         printf("Result is: %d + %d = %d\n",op1, op2, result);
+         break;
+        case '-':result=op1 - op2;
+                printf("Result is: %d - %d = %d\n",op1, op2, result);
+                break;
+        case '*':result=op1 * op2;
+                printf("Result is: %d * %d = %d\n",op1, op2, result);
+                break;
+        case '/':result=op1 / op2;
+                printf("Result is: %d / %d = %d\n",op1, op2, result);
+                break;
+        default: 
+                printf("ERROR: Unsupported Operation");
+    }
+  
+write(connfd,&result,sizeof(result));   
+close(sockfd);
+}
+
+/*
+gescoe@gescoe-OptiPlex-3020:~/Desktop/TEA84$ cd TCP
+gescoe@gescoe-OptiPlex-3020:~/Desktop/TEA84/TCP$ gcc scalculator.c
+gescoe@gescoe-OptiPlex-3020:~/Desktop/TEA84/TCP$ ./a.out
+socket created sucessfully
+listen sucessful
+^Z
+[1]+  Stopped                 ./a.out
+gescoe@gescoe-OptiPlex-3020:~/Desktop/TEA51/TCP$ gcc scalculator.c
+gescoe@gescoe-OptiPlex-3020:~/Desktop/TEA51/TCP$ ./a.out
+socket created sucessfully
+bind sucessful
+listen sucessful
+accept sucessful
+Result is: 22 + 45 = 67
+gescoe@gescoe-OptiPlex-3020:~/Desktop/TEA51/TCP$ 
+gescoe@gescoe-OptiPlex-3020:~/Desktop/TEA51/TCP$ gcc scalculator.c
+gescoe@gescoe-OptiPlex-3020:~/Desktop/TEA51/TCP$ ./a.out
+socket created sucessfully
+bind sucessful
+listen sucessful
+accept sucessful
+Result is: 54 * 20 = 1080
+gescoe@gescoe-OptiPlex-3020:~/Desktop/TEA51/TCP$ 
+*/
